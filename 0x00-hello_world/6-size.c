2df4fc8c3d8a29a4a20a14a74a71ad1946d655ce
tkundu06 <tonykundu06@gmail.com>
2023-09-12 00:08:56 +0000

    Various output size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ int main(void)
 	char m;
 	float n;
 
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
 	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(m));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(n));
