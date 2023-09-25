8c9960920d2bcd9f9b445836fa8bb1ca8977d852
tkundu06 <tonykundu06@gmail.com>
2023-09-25 20:01:50 +0000

    Function taking pointer int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,26 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * reset_to_98 - set the value of int to 98
  *
  * Return: nothing
  */
-void reset_to_98(int *n);
-/**
- * main - modify a variable n from outside
- *
- * Return: Always 0 Success
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
 void reset_to_98(int *n)
 {
 	*n = 98;
