8a5f250cbb635d71a5c3d03e433f1734be4f6f45
tkundu06 <tonykundu06@gmail.com>
2023-10-09 23:56:25 +0000

    Prints sizes of various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the sizes of various data types
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(unsigned int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(unsigned long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(unsigned long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
