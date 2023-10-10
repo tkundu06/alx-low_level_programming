df9e2f399de03098bb24ea3bb32967d4d2750119
tkundu06 <tonykundu06@gmail.com>
2023-10-10 02:40:42 +0000

    Final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 
 /**
  * main - Program to print string
@@ -10,6 +9,6 @@
 int main(void)
 {
 	write(2,
-			" and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
