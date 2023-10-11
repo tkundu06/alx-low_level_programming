d1a5f03bf1ec181892abd2c83866c9009d724bbd
tkundu06 <tonykundu06@gmail.com>
2023-10-11 16:05:43 +0000

    Function to print letters of alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - void print_alphabet(void) function called here
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - function to print letters of alphabet
+ *
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	putchar('\n');
+}
