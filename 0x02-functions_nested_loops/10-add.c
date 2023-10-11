577613923e69cf226cae89e913a249594224b394
tkundu06 <tonykundu06@gmail.com>
2023-10-11 21:44:29 +0000

    Function that adds two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,16 +2,15 @@
 
 /**
  * add - Function add two integers and returns the results
- * @int: First parameter of type int
- * @int: Second parameter of type int
+ * @a: First parameter of type int
+ * @b: Second parameter of type int
  *
  * Return: nothing
  */
-int add(int, int)
+int add(int a, int b)
 {
-	int a;
-	int b;
-	int n = a + b;
+	int n;
 
+	n = a + b;
 	return (n);
 }
