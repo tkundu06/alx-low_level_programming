20944927b004b20a368555dc90aaa681c88d1449
tkundu06 <tonykundu06@gmail.com>
2023-10-11 15:25:07 +0000

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	_putchar('_');
-	('p');
-	('u');
-	('t');
-	('c');
-	('h');
-	('a');
-	('r');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	('\n');
 
 	return (0);
