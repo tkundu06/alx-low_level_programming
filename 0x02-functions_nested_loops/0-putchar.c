da6c88f93d470ee7f97cb37653d11bf3bd2d09ae
tkundu06 <tonykundu06@gmail.com>
2023-10-11 14:51:19 +0000

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+***0x02. C - Functions, nested loops***
+The project will cover:
+1. Functions
+2. Nested Loops
