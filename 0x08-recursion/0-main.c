753666c1f17f867b6efa09a39ff331f4f1c1a5f5
tkundu06 <tonykundu06@gmail.com>
2024-02-14 14:24:25 +0300

    Recursive function to print string, followed by a new line

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - The function will call and recursive function code
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,2 +1,22 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - recursive function to print the string
+ * @s: the pointer to hold all the characters that will be printed
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (!s || !*s)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
+
