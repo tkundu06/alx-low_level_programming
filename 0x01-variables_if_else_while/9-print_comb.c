eb40ca32061d1afe3265d2df34eca551dd31a013
tkundu06 <tonykundu06@gmail.com>
2024-05-02 14:55:38 +0000

    Program to print all single digits in combination with commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to our program
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int d = 0;
+
+	while (0 <= 9)
+	{
+		putchar('0' + d);
+		if (d == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		d++;
+	}
+
+	return (0);
+}
