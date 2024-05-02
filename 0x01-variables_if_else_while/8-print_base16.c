8623d9c505940626d222fb47436a63a755aefc72
tkundu06 <tonykundu06@gmail.com>
2024-05-02 12:20:06 +0000

    Code to print hexadecimal in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to our program; print all hexadecimal numbers in lowercase
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int ch = 0x0;
+
+	while (ch <= 0xF)
+	{
+		printf("%x", ch);
+		if (ch == 0xF)
+		{
+			printf("\n");
+			break;
+		}
+		ch++;
+	}
+
+	return (0);
+}
