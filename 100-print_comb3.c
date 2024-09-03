01ffeb4310920e8518977a42abcd53432b99b506
tkundu06 <tonykundu06@gmail.com>
2024-09-03 11:11:28 +0000

    Program to print combination of numbers

--- 100-print_comb3.c
+++ 100-print_comb3.c
@@ -9,27 +9,27 @@
 int main(void)
 {
 	/*Variable declaration and initialization*/
-	int x = 1;
-	int start = 0; /*Starting number for first iteration*/
-	int count = 9; /*Number of times to print initial number*/
+	int x = '1';
+	int start = '0'; /*Starting number for first iteration*/
+	int count = '9'; /*Number of times to print initial number*/
 
 
-	while (x <= 9)
+	while (x <= '9')
 	{
 		int y = x;
 		int num = start;
 		int times = count;
 
-		while (y <= 9 && times > 0)
+		while (y <= '9' && times > '0')
 		{
-			printf("%d", num);
-			printf("%d", y);
-			if (x == 9)
+			putchar(num);
+			putchar(y);
+			if (x == '9')
 			{
 				break;
 			}
-			printf(",");
-			printf(" ");
+			putchar(',');
+			putchar(' ');
 			times--;
 			y++;
 		}
@@ -38,7 +38,7 @@ int main(void)
 		x++;
 	}
 
-	printf("\n");
+	putchar('\n');
 
 	/*Return zero to the main() calling function*/
 	return (0);
